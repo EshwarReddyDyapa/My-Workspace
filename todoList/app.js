6879803dc48ddab35e7cedeabb438db9c1621424
EshwarReddyDyapa <eshwarreddyder@gmail.com>
2023-02-06 23:20:03 +0530

    Complete EJS template turorial

--- todoList/app.js
+++ todoList/app.js
@@ -4,18 +4,42 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
 
-var items = [];
+const options = { month: 'short', day: 'numeric', weekday: 'long'}
+
+let dateToDisplay;
+let date = new Date();
+
+dateToDisplay = date.toLocaleDateString('en-IN', options);
+
+var items = ['First item', 'Second ToDo item'];
+let workItems = [];
 
 app.get('/', (req,res) => {
-    res.render('index', {listItem: items});
+    res.render('index', {listItem: items, dateToDisplay: dateToDisplay});
 });
 
-app.post('/', (req,res) => {
-    // console.log(req.body.listItem);
-    items.push(req.body.listItem);
-    res.redirect('/');
+app.get('/work', (req,res) => {
+    res.render('index', {listItem: workItems, dateToDisplay: 'Work - ToDo'});
+});
+
+app.get('/about', (req,res) => {
+    res.render('about');
 })
+
+app.post('/', (req,res) => {
+    console.log(req.body);
+    if(req.body.button === 'Work - ToDo '){
+        workItems.push(req.body.listItem);
+        //res.render('index', {listItem: workItems, dateToDisplay: 'Work - ToDo'});
+        res.redirect('/work');
+    }
+    else{
+        items.push(req.body.listItem);
+        res.redirect('/');
+    }
+});
 
 app.listen(3000, () =>{
     console.log("Server runnning on port 3000");
